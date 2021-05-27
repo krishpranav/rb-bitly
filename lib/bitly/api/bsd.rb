4b3027deed8cd687d74935d3438058e78a0c5aba
krishpranav <krisna.pranav@gmail.com>
2021-05-27 12:53:28 +0530

    [+] bsd functions [+]

---
+++ lib/bitly/api/bsd.rb
@@ -0,0 +1,15 @@
+require_relative "./list"
+
+module Bitly
+    module API
+        module BSD
+            class List < Bitly::API::List ; end
+
+            def self.list(client:)
+                response = client.request(path: "/bsds")
+                bsds = response.body["bsds"]
+                List.new(items: bsds, response: response)
+            end
+        end
+    end
+end
