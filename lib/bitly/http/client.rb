0dc9dbaafe38393f050fbcda0611449e631174f0
krishpranav <krisna.pranav@gmail.com>
2021-05-27 19:22:37 +0530

    [+] client update [+]

--- lib/bitly/http/client.rb
+++ lib/bitly/http/client.rb
@@ -6,4 +6,16 @@ module Bitly
                 raise ArgumentError, "Adapter must have a request method", unless @adapther.respond_to?(:request)
 
             end
-            
+
+            def request(request)
+                status, body, headers, success = @adapther.request(request)
+                response = Bitly::HTTP::Response.new(status: status, body: body, headers: headers, request: request)
+                if success
+                    return response
+                else
+                    raise Bitly::Error, response
+                end
+            end
+        end
+    end
+end
