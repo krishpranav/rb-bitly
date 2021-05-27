c8d18a3c4e62b4abd6f6c0b1273effefb1b602ab
krishpranav <krisna.pranav@gmail.com>
2021-05-27 12:46:16 +0530

    [+] update self shorten functions [+]

--- lib/bitly/api/bitlink.rb
+++ lib/bitly/api/bitlink.rb
@@ -11,4 +11,19 @@ module Bitly
             autoload :LinkClick, File.join(File.dirname(__FILE__), "bitlink/link_click.rb")
 
             include Base
-            
+
+            class List < Bitly::API::List ; end
+
+
+            def self.shorten(client:, long_url:, domain: nil, group_guid: nil)
+                response = client.request(
+                    path: "/shorten",
+                    method: "POST",
+                    params: {
+                        "long_url" => long_url,
+                        "domain" => domain,
+                        "group_guid" => group_guid
+                    })
+                new(data: response.body, client: client, response: respond)
+                    end
+                    
