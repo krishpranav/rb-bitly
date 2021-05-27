3eac4b8cf4a00a2ed68ce7388369bf581c585c3f
krishpranav <krisna.pranav@gmail.com>
2021-05-27 11:36:02 +0530

    [+] added some functions [+]

--- spec/bitly/error_spec.rb
+++ spec/bitly/error_spec.rb
@@ -7,3 +7,16 @@ RSpec.describe Bitly::Error do
         }.to_json,
         headers: {}
     )}
+
+    it "is initialised with a Bitly::HTTP::Response" do
+        expect { Bitly::Error.new}.to raise_error(ArgumentError)
+        expect { Bitly::Error.new(response) }.not_to raise_error
+    end
+
+    describe "with an error" do
+        let(:error) { Bitly::Error.new(response)}
+
+        it "returns the original response" do
+            expect(error.response).to eq(response)
+        end
+        
